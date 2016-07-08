0076d05d4dc570ce1fbae70daa4eb975809825be
Chong Zhang <chongz@chromium.org>
2016-07-08 12:19:27 -0400

    Format logging, add listener for 'input' event

--- logger.js
+++ logger.js
@@ -1,27 +1,59 @@
 (function() {
 
+    // Vars.
     var eventLog = document.getElementById('eventLog');
     var HTMLLog = document.getElementById('HTMLLog');
+    var logCounter = 1;
 
+    // Methods.
     function escapeHTML(html) {
         return html.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     }
 
-    function inputEventToString(event) {
-        var s = 'InputEvent - ';
-        ['type', 'inputType', 'data', 'isComposing'].forEach(function(val) {
-            s += val + ':<b>' + event[val] + '</b>&nbsp;&nbsp;';
-        })
-        return s + `getRanges():${event.getRanges()}`;
+    function paddingToLength(str, len) {
+        const spaces = Math.max(0, len - str.length);
+        return str + '&nbsp;'.repeat(spaces);
     }
 
-    document.addEventListener('beforeinput', function(event) {
-        eventLog.innerHTML += inputEventToString(event) + '</br>';
-    });
+    function inputEventToString(event) {
+        const typeStr = `type:<span class="type type-${event.type}">${event.type}</span>`;
+        const inputTypeStr = `inputType:<span class="inputtype">${event.inputType}</span>`;
+        const dataStr = `data:<span class="data">"${event.data}"</span>`;
+        const isComposingStr = `isComposing:<span class="is-composing">${event.isComposing}</span>`;
+        // Ranges.
+        const ranges = event.getRanges();
+        var rangeStr = '';
+        for (var i = 0; i < ranges.length; ++i) {
+            const range = ranges[i];
+            rangeStr += `("${range.startContainer.textContent}":${range.startOffset}, "${range.endContainer.textContent}":${range.endOffset}), `;
+        }
+        rangeStr = `getRanges():[${rangeStr}]`;
 
-    document.addEventListener('input', function(event) {
+        const eventStr = `InputEvent - ${typeStr} ${inputTypeStr} ${dataStr} ${isComposingStr} ${rangeStr}`;
+        return eventStr;
+    }
+
+    function appendLog(str) {
+        eventLog.innerHTML = `<span class="log-counter">${logCounter}.</span> ${str}</br> ${eventLog.innerHTML}`;
+        ++logCounter;
+    }
+
+    function refreshHTMLLog() {
         HTMLLog.innerHTML = escapeHTML(editor.innerHTML);
-    });
-    HTMLLog.innerHTML = escapeHTML(editor.innerHTML);
+    }
 
+    // Event listeners.
+    document.addEventListener('beforeinput', event => {
+        console.log(event);
+        appendLog(inputEventToString(event));
+    });
+
+    document.addEventListener('input', event => {
+        console.log(event);
+        appendLog(inputEventToString(event));
+        refreshHTMLLog();
+    });
+
+    // Start.
+    refreshHTMLLog();
 })();
